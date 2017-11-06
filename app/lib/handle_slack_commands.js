3a34c6d43be10690cfab8e50d059d59329b02a99
Findley, Randy <rgfindley@gmail.com>
2017-11-06 12:01:22 -0500

    Add command stubs for the other commands.

--- app/lib/handle_slack_commands.js
+++ app/lib/handle_slack_commands.js
@@ -4,9 +4,47 @@ const _ = require('lodash');
 
 const slack = require('./slack');
 
-const VALID_COMMANDS = ['create project', 'deploy fn', 'show fn', 'add fn'];
+const VALID_COMMANDS = ['create project', 'delete project', 'show project', 'show projects', 'deploy fn', 'show fn', 'add fn', 'show fns'];
 
 var functions = {};
+
+functions.add_fn = function(params, callback) {
+    if (_.isEqual(_.toLower(_.head(params.options)), 'help')) {
+        return callback(null, {
+            statusCode: 200,
+            body: 'Use this command to add a new function.  Usage: /fn add fn <short-name> <lambda-arn>'
+        });
+    }
+
+};
+
+functions.delete_fn = function(params, callback) {
+    if (_.isEqual(_.toLower(_.head(params.options)), 'help')) {
+        return callback(null, {
+            statusCode: 200,
+            body: 'Use this command to delete a function.  Usage: /fn delete fn <short-name>'
+        });
+    }
+};
+
+functions.show_fn = function(params, callback) {
+    if (_.isEqual(_.toLower(_.head(params.options)), 'help')) {
+        return callback(null, {
+            statusCode: 200,
+            body: 'Use this command to show a functions deployment history.  Usage: /fn show fn <short-name> '
+        });
+    }
+};
+
+functions.show_fns = function(params, callback) {
+    if (_.isEqual(_.toLower(_.head(params.options)), 'help')) {
+        return callback(null, {
+            statusCode: 200,
+            body: 'Use this command to show all functions.  Usage: /fn show fns'
+        });
+    }
+
+};
 
 functions.create_project = function(params, callback) {
     console.log('handle_slack_commands.create_project');
@@ -37,6 +75,36 @@ functions.create_project = function(params, callback) {
             });
         }
     });
+};
+
+functions.delete_project = function(params, callback) {
+    if (_.isEqual(_.toLower(_.head(params.options)), 'help')) {
+        return callback(null, {
+            statusCode: 200,
+            body: 'Use this command to delete a project.  Usage: /fn delete project <github_repo-github_branch>'
+        });
+    }
+
+};
+
+functions.show_project = function(params, callback) {
+    if (_.isEqual(_.toLower(_.head(params.options)), 'help')) {
+        return callback(null, {
+            statusCode: 200,
+            body: 'Use this command to show the build history of a project.  Usage: /fn show project <github_repo-github_branch>'
+        });
+    }
+
+};
+
+functions.show_projects = function(params, callback) {
+    if (_.isEqual(_.toLower(_.head(params.options)), 'help')) {
+        return callback(null, {
+            statusCode: 200,
+            body: 'Use this command to show all projects.  Usage: /fn show projects'
+        });
+    }
+
 };
 
 functions.handle = function(body, callback) {
